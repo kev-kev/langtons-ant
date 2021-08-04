cd13f55ae6c397e80362eb6f3636c267e718b4fd
kev-kev <kevin.clark93@gmail.com>
2021-08-04 19:03:58 -0400

    adjusting math since origin is in the top left of canvas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
   const canvas = document.getElementById("myCanvas");
   window.ctx = canvas.getContext("2d");
   const game = new Game();
-  setInterval(() => game.moveAnt(), 100);
+  setInterval(() => game.moveAnt(), 1000);
 });
 
 // canvas size is 500x500, so we have a 50x50 grid of 10px boxes
@@ -10,7 +10,6 @@ const BOX_SIZE = 10;
 
 class Game {
   constructor() {
-    this.boxes = [];
     this.curBox = [25, 25]; // start in middle
     this.direction = 0; // 0 degrees = north, 90 = east, etc...
   }
@@ -21,24 +20,29 @@ class Game {
 
     // need to determine if the current box is filled or not and the coords of the box in front of the ant
     // then, flip the box, rotate the ant, and move forward
+
     isCoordinateFilled(this.curBox)
       ? (this.direction -= 90)
       : (this.direction += 90);
+
+    console.log("After:", this.direction);
     if (Math.abs(this.direction) === 360) this.direction = 0;
     flipBox(this.curBox);
-    this.curBox = this.getBoxInFront();
+    const nextBox = this.getBoxInFront();
+    console.log("Next box: ", nextBox);
+    this.curBox = nextBox;
   }
 
   getBoxInFront() {
     switch (this.direction) {
       case 0:
-        return [this.curBox[0], this.curBox[1] + 1];
+        return [this.curBox[0], this.curBox[1] - 1];
       case 90:
       case -270:
         return [this.curBox[0] + 1, this.curBox[1]];
       case 180:
       case -180:
-        return [this.curBox[0], this.curBox[1] - 1];
+        return [this.curBox[0], this.curBox[1] + 1];
       case 270:
       case -90:
         return [this.curBox[0] - 1, this.curBox[1]];
@@ -57,11 +61,7 @@ const isCoordinateFilled = (coords) => {
 const flipBox = (coords) => {
   let x, y;
   [x, y] = coords;
-  if (isCoordinateFilled(coords)) {
-    clearBox(x, y);
-  } else {
-    fillBox(x, y);
-  }
+  isCoordinateFilled(coords) ? clearBox(x, y) : fillBox(x, y);
 };
 
 const fillBox = (x, y, color = "black") => {
