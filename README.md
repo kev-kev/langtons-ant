804f4452c7be246154232138edf4434e93d6e85d
kev-kev <kevin.clark93@gmail.com>
2021-08-05 10:16:57 -0400

    finishing touches

---
+++ README.md
@@ -0,0 +1,15 @@
+# Langton's Ant
+
+<img src="https://cdn.glitch.com/5d23f082-2afb-4cba-8da4-832cb638e896%2FScreen%20Shot%202021-08-05%20at%209.37.25%20AM.png?v=1628170667285" />
+
+An implementation of Langton's Ant, made using HTML5 Canvas
+
+[More Info](https://mathworld.wolfram.com/LangtonsAnt.html)
+
+## Rules
+
+```
+At a white square, turn 90° clockwise, flip the color of the square, move forward one unit
+
+At a black square, turn 90° counter-clockwise, flip the color of the square, move forward one unit
+```
--- script.js
+++ script.js
@@ -2,15 +2,15 @@ window.addEventListener("load", () => {
   const canvas = document.getElementById("myCanvas");
   window.ctx = canvas.getContext("2d");
   const game = new Game();
-  setInterval(() => game.moveAnt(), 1000);
+  setInterval(() => game.moveAnt(), 1);
 });
 
-// canvas size is 500x500, so we have a 50x50 grid of 10px boxes
-const BOX_SIZE = 10;
+// canvas size is 500x500, so using 5px boxes gives us a 100x100 grid
+const BOX_SIZE = 5;
 
 class Game {
   constructor() {
-    this.curBox = [25, 25]; // start in middle
+    this.curBox = [50, 50]; // start in middle
     this.direction = 0; // 0 degrees = north, 90 = east, etc...
   }
 
@@ -18,18 +18,13 @@ class Game {
     // if on an empty box, turn 90 CCW, flip the color, move forward
     // if on a filled box, turn 90 CW, flip the color, move forward
 
-    // need to determine if the current box is filled or not and the coords of the box in front of the ant
-    // then, flip the box, rotate the ant, and move forward
-
-    isCoordinateFilled(this.curBox)
+    this.isCoordinateFilled(this.curBox, this.direction)
       ? (this.direction -= 90)
       : (this.direction += 90);
 
-    console.log("After:", this.direction);
     if (Math.abs(this.direction) === 360) this.direction = 0;
-    flipBox(this.curBox);
+    this.flipBox(this.curBox, this.direction);
     const nextBox = this.getBoxInFront();
-    console.log("Next box: ", nextBox);
     this.curBox = nextBox;
   }
 
@@ -48,27 +43,26 @@ class Game {
         return [this.curBox[0] - 1, this.curBox[1]];
     }
   }
+
+  isCoordinateFilled(coords) {
+    let x, y;
+    [x, y] = coords;
+    const imageData = window.ctx.getImageData(x * BOX_SIZE, y * BOX_SIZE, 1, 1);
+    return imageData["data"]["3"] > 0;
+  }
+
+  flipBox(coords) {
+    let x, y;
+    [x, y] = coords;
+    this.isCoordinateFilled(coords) ? this.clearBox(x, y) : this.fillBox(x, y);
+  }
+
+  fillBox(x, y, color = "black") {
+    window.ctx.fillStyle = color;
+    window.ctx.fillRect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
+  }
+
+  clearBox(x, y) {
+    window.ctx.clearRect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
+  }
 }
-
-// returns true if any color other than white
-const isCoordinateFilled = (coords) => {
-  let x, y;
-  [x, y] = coords;
-  const imageData = window.ctx.getImageData(x * BOX_SIZE, y * BOX_SIZE, 1, 1);
-  return imageData["data"]["0"] > 0;
-};
-
-const flipBox = (coords) => {
-  let x, y;
-  [x, y] = coords;
-  isCoordinateFilled(coords) ? clearBox(x, y) : fillBox(x, y);
-};
-
-const fillBox = (x, y, color = "black") => {
-  window.ctx.fillStyle = color;
-  window.ctx.fillRect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
-};
-
-const clearBox = (x, y) => {
-  window.ctx.clearRect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
-};
