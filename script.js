f43ea62dcd0590586b838d06387550f803a21459
kev-kev <kevin.clark93@gmail.com>
2021-10-26 12:53:23 -0400

    creating class for ant

--- script.js
+++ script.js
@@ -2,45 +2,59 @@ window.addEventListener("load", () => {
   const canvas = document.getElementById("myCanvas");
   window.ctx = canvas.getContext("2d");
   const game = new Game();
-  setInterval(() => game.moveAnt(), 1);
+  for (let i = 0; i < game.ants.length; i++) {
+    setInterval(() => game.ants[i].move(), 20);
+  }
 });
 
 // canvas size is 500x500, so using 5px boxes gives us a 100x100 grid
-const BOX_SIZE = 5;
+const BOX_SIZE = 2;
+
+document.getElementById("newAntForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dir = document.getElementById("dirSelect").value;
+  const x = document.getElementById("x").value;
+  const y = document.getElementById("y").value;
+});
 
 class Game {
   constructor() {
-    this.curBox = [50, 50]; // start in middle
-    this.direction = 0; // 0 degrees = north, 90 = east, etc...
+    this.ants = [new Ant([125, 125], 0)];
   }
+}
 
-  moveAnt() {
+class Ant {
+  constructor(coords, dir) {
+    this.coords = coords;
+    this.dir = dir;
+  }
+  move() {
     // if on an empty box, turn 90 CCW, flip the color, move forward
     // if on a filled box, turn 90 CW, flip the color, move forward
 
-    this.isCoordinateFilled(this.curBox, this.direction)
-      ? (this.direction -= 90)
-      : (this.direction += 90);
+    this.isCoordinateFilled(this.coords, this.dir)
+      ? (this.dir -= 90)
+      : (this.dir += 90);
 
-    if (Math.abs(this.direction) === 360) this.direction = 0;
-    this.flipBox(this.curBox, this.direction);
+    if (Math.abs(this.dir) === 360) this.dir = 0;
+    this.flipBox(this.coords, this.dir);
     const nextBox = this.getBoxInFront();
-    this.curBox = nextBox;
+    this.coords = nextBox;
   }
 
   getBoxInFront() {
-    switch (this.direction) {
+    switch (this.dir) {
       case 0:
-        return [this.curBox[0], this.curBox[1] - 1];
+        return [this.coords[0], this.coords[1] - 1];
       case 90:
       case -270:
-        return [this.curBox[0] + 1, this.curBox[1]];
+        return [this.coords[0] + 1, this.coords[1]];
       case 180:
       case -180:
-        return [this.curBox[0], this.curBox[1] + 1];
+        return [this.coords[0], this.coords[1] + 1];
       case 270:
       case -90:
-        return [this.curBox[0] - 1, this.curBox[1]];
+        return [this.coords[0] - 1, this.coords[1]];
     }
   }
 
